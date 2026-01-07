f959c03ff752f71b4845986c626df6c00aa0f6bd
Daniel Lee <lihongyangnju@gmail.com>
2026-01-07 22:01:03 +0800

    Initial Commit

---
+++ Focusr - ADHD浏览器/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focusr - ADHD浏览器
+//
+//  Created by Mercury on 2026/1/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focusr - ADHD浏览器/Focusr___ADHD___App.swift
@@ -0,0 +1,17 @@
+//
+//  Focusr___ADHD___App.swift
+//  Focusr - ADHD浏览器
+//
+//  Created by Mercury on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct Focusr___ADHD___App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
